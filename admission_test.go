6df613db72df85f1ad0660461e503533bc2d7b44
Artur Rodrigues <arturhoo@gmail.com>
2023-11-28 08:43:08 +0000

    Implement admission_test

---
+++ admission_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+
+	corev1 "k8s.io/api/core/v1"
+)
+
+func TestAddMultiarchTolerationToPod(t *testing.T) {
+	pod := &corev1.Pod{
+		Spec: corev1.PodSpec{
+			Tolerations: []corev1.Toleration{
+				{
+					Key:      "key1",
+					Operator: corev1.TolerationOpEqual,
+					Value:    "value1",
+				},
+				{
+					Key:      "key2",
+					Operator: corev1.TolerationOpEqual,
+					Value:    "value2",
+				},
+			},
+		},
+	}
+	AddMultiarchTolerationToPod(pod)
+	expectedTolerations := []corev1.Toleration{
+		{
+			Key:      "key1",
+			Operator: corev1.TolerationOpEqual,
+			Value:    "value1",
+		},
+		{
+			Key:      "key2",
+			Operator: corev1.TolerationOpEqual,
+			Value:    "value2",
+		},
+		MultiarchToleration,
+	}
+
+	if !reflect.DeepEqual(pod.Spec.Tolerations, expectedTolerations) {
+		t.Errorf("Unexpected tolerations. Expected: %v, Got: %v", expectedTolerations, pod.Spec.Tolerations)
+	}
+}
